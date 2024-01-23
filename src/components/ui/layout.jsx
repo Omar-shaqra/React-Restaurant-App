65508ce6dfaf4cbfe495bdf2de548b40aed0a153
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-01-23 18:49:21 +0200

    feat: added footer

--- src/components/ui/layout.jsx
+++ src/components/ui/layout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../navbar";
+import Footer from "../footer";
 function Layout() {
   return (
-    <div className="flex flex-col flex-1">
-      <Navbar />
-      <div className="flex-1 p-4 min-h-0 overflow-auto">
-        <Outlet />
+    <div className="flex flex-row h-screen w-screen overflow-hidden">
+      <div className="flex flex-col flex-1">
+        <Navbar />
+        <div className="flex-1 p-4 min-h-0 overflow-auto">
+          <Outlet />
+          <Footer />
+        </div>
       </div>
     </div>
   );
