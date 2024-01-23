416c05f3366ec6a83b8a1af12bd49d2fa795d58d
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-01-23 23:41:13 +0200

    chore: build test

--- src/components/ui/category-card.jsx
+++ src/components/ui/category-card.jsx
@@ -1,7 +1,7 @@
 const CategoryCard = ({ img, title }) => {
   return (
     <div className="flex flex-col items-center">
-      <img className="w-40" src="/src/assets/cate1.png" alt="" />
+      <img className="w-40" src="/src/assets/cate1.png" alt="category img" />
       <h3 className="font-semibold"> fresh drinks</h3>
     </div>
   );
