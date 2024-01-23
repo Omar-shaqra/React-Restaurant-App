d93d795337f9235239943f78870f676584d5e6b0
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-01-23 05:48:45 +0200

    feat: get user state from clerk

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,38 @@
-import { UserButton } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+import { UserRoundPlus } from "lucide-react";
+import { UserButton, useUser, SignedIn } from "@clerk/clerk-react";
+
 import Container from "./ui/Container";
 import NavCart from "./cart/nav-cart";
-
 function Navbar() {
+  const { isSignedIn } = useUser();
+
   return (
-    <div className="border-b mb-2 relative">
+    <div className="border-b border-b-orange-700 mb-2 relative">
       <Container>
         <div className="flex h-16 items-center px-4">
           <a href="/">
-            <img src="/public/logo.png" className="w-16 h-auto mr-8" />
+            <img src="/src/assets/logo.png" className="w-16 h-auto mr-8" />
           </a>
-          <div className="flex items-center space-x-4">
-            <p>Item</p>
+          <div className="flex items-center space-x-4 text-lg font-semibold text-orange-900">
+            <a href="#deals" className="hover:scale-105 transition">
+              Deals
+            </a>
             <p>Item</p>
             <p>Item</p>
             <p>Item</p>
           </div>
           <div className="ml-auto flex items-center space-x-4">
             <NavCart />
-            <UserButton afterSignOutUrl="/" />
+            {isSignedIn ? (
+              <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+              </SignedIn>
+            ) : (
+              <Link to={"/admin"}>
+                <UserRoundPlus size={20} />
+              </Link>
+            )}
           </div>
         </div>
       </Container>
