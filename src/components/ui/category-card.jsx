91e693e19d9288893e5b080bd7f056d375b7a1e3
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-01-24 02:31:39 +0200

    fix: img import

--- src/components/ui/category-card.jsx
+++ src/components/ui/category-card.jsx
@@ -1,7 +1,9 @@
+import cateImg from "../../assets/cate1.png";
+
 const CategoryCard = ({ img, title }) => {
   return (
     <div className="flex flex-col items-center">
-      <img className="w-40" src="/assets/cate1.png" alt="category img" />
+      <img className="w-40" src={cateImg} alt="category img" />
       <h3 className="font-semibold"> fresh drinks</h3>
     </div>
   );
