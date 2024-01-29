1f8823374db9adbc03aef08caeb438a730eed341
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-01-29 19:15:47 +0200

    refactor: delete unused files

--- src/actions/getSubcategory.js
+++
@@ -1,9 +0,0 @@
-const URL = import.meta.env.PUBLIC_API_URL
-
-export const getSubcategory = async () => {
-  const res = await fetch(URL);
-
-  return res.json();
-};
-
-export default getSubcategory;
