5b8d4f718aa5e11f40dcfe1bbfb7f2ff30e4c68a
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-03-04 13:56:24 +0200

    feat: sort products by category

--- src/actions/get-products.jsx
+++ src/actions/get-products.jsx
@@ -7,7 +7,7 @@ export const GetProducts = ({ setProducts }) => {
     queryFn: async () => {
       const { data } = await axios.get(
         // `https://restaurant-menue-ordering-v1.onrender.com/api/v1/products`
-        `http://localhost:8000/api/v1/products`
+        `http://localhost:8000/api/v1/products?sort=category`
       );
       setProducts(data);
       return data;
