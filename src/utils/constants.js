f480b23ad55bbbe18cebb1d928b23cea0d2d9856
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-04-18 13:22:50 +0200

    build: fix unfound assests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 // import slide1 from "/src/assets/slide1.jpg";
-import slide2 from "/src/assets/slide2.jpg";
+// import slide2 from "/src/assets/slide2.jpg";
 // import slide3 from "/src/assets/slide3.jpg";
-import slide4 from "/src/assets/slide4.jpg";
+// import slide4 from "/src/assets/slide4.jpg";
 // import slide5 from "/src/assets/slide5.jpg";
-export const heroSlides = [slide2, slide4];
+// export const heroSlides = [slide2, slide4];
 
 function checkUserRole(session) {
   if (
