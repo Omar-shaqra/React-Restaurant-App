d5967b2d6b15d0205986704fa49139abe8d86eeb
Abdelrahman-Reda-Dawood <116512972+Abdelrahman-Reda-Dawood@users.noreply.github.com>
2024-04-26 12:59:49 +0300

    style

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,11 @@
-import { Facebook, Instagram, Youtube, MapPin, Phone } from "lucide-react";
+import {
+  Facebook,
+  Instagram,
+  Youtube,
+  MapPin,
+  Phone,
+  MailPlus,
+} from "lucide-react";
 import { Link } from "react-router-dom";
 import ContactModal from "./modals/contact-modal";
 import { useState } from "react";
@@ -10,146 +17,82 @@ const Footer = () => {
     <section
       id="footer"
       className="flex items-center justify-center mt-10 font-serif font-semibold text-white bg-gradient-to-t from-black to-neutral-900/30">
-      <div className="flex-1 w-full pt-5 mx-auto">
+      <div className="flex-1 w-full mx-auto">
         {/* Logo & Description */}
-        <div className="px-4">
-          <p className="flex items-center gap-3 mb-1 text-xl font-bold text-justify w-fit sm:text-3xl sm:text-left">
-            <img src={"/logo.png"} alt="Logo" className="max-w-[80px] bg" />A
-            Pizza
+        <div className="flex items-center justify-around gap-5 p-4 text-lg font-bold sm:text-2xl">
+          <p className="font-mono text-center text-slate-200">
+            مصنع السعادة أي بيتزا يرحب بكم...زورونا في جميع ولايات السلطنة
           </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Possimus, voluptate
-          </p>
-          <br />
+          <img
+            src={"/logo.png"}
+            alt="Logo"
+            className="max-w-[80px] px-4 rounded-full bg-white"
+          />
         </div>
 
         <div className="flex flex-col justify-between w-full border-t md:flex-row">
           {/* Branches */}
           <div className="flex flex-col items-center px-4 py-8">
             <p className="p-2 text-xl font-bold text-justify rounded-lg w-fit sm:text-xl text-nowrap sm:text-left">
-              Branches
+              Branches - الفروع
             </p>
-            <ul className="flex flex-col items-center justify-center gap-1 cursor-pointer w-fit">
-              <Link
-                to="https://maps.app.goo.gl/3HL5zc51PHwRkSxm7?g_st=ic"
-                target="_blank"
-                className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
-                <MapPin />
-                <li className="rounded-lg ">Al Khuwair - الخوير</li>
-              </Link>
-              <Link
-                to="https://maps.app.goo.gl/uKJeFEAmiMPxk4yq9?g_st=ic"
-                target="_blank"
-                className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
-                <MapPin />
-                <li className="rounded-lg ">AL Amrat - العامرات</li>
-              </Link>
-              <Link
-                to="https://maps.app.goo.gl/r2QxwWHhrB4N1zq87?g_st=ic"
-                target="_blank"
-                className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
-                <MapPin />
-                <li className="rounded-lg ">Al-Watiya - الوطية</li>
-              </Link>
-              <Link
-                to="https://maps.app.goo.gl/F2cPp2gnTv41ydCR6?g_st=ic"
-                target="_blank"
-                className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
-                <MapPin />
-                <li className="rounded-lg ">Al Khawd - الخوض</li>
-              </Link>
-              <Link
-                to="https://maps.app.goo.gl/YUdogHGcMKeWYgYQA?g_st=ic"
-                target="_blank"
-                className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
-                <MapPin />
-                <li className="rounded-lg ">Al Ma`abilah - المعبيلة</li>
-              </Link>
-            </ul>
+            <Link
+              to="https://maps.app.goo.gl/3HL5zc51PHwRkSxm7?g_st=ic"
+              target="_blank"
+              className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
+              <MapPin />
+              <p className="rounded-lg ">Al Khuwair - الخوير</p>
+            </Link>
+
+            <Link
+              to="https://maps.app.goo.gl/uKJeFEAmiMPxk4yq9?g_st=ic"
+              target="_blank"
+              className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
+              <MapPin />
+              <p className="rounded-lg ">AL Amrat - العامرات</p>
+            </Link>
+
+            <Link
+              to="https://maps.app.goo.gl/r2QxwWHhrB4N1zq87?g_st=ic"
+              target="_blank"
+              className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
+              <MapPin />
+              <p className="rounded-lg ">Al-Watiya - الوطية</p>
+            </Link>
+
+            <Link
+              to="https://maps.app.goo.gl/F2cPp2gnTv41ydCR6?g_st=ic"
+              target="_blank"
+              className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
+              <MapPin />
+              <p className="rounded-lg ">Al Khawd - الخوض</p>
+            </Link>
+
+            <Link
+              to="https://maps.app.goo.gl/YUdogHGcMKeWYgYQA?g_st=ic"
+              target="_blank"
+              className="flex items-center gap-2 p-2 transition-colors duration-300 rounded-lg text-nowrap hover:text-orange-400">
+              <MapPin />
+              <p className="rounded-lg ">Al Ma`abilah - المعبيلة</p>
+            </Link>
           </div>
 
           {/* Links */}
-          <div className="flex flex-col items-center px-4 py-8">
-            <p className="p-2 mb-3 text-xl font-bold text-justify rounded-lg w-fit sm:text-xl sm:text-left">
-              Links
+          <div className="flex flex-col items-center gap-4 px-4 py-8">
+            <p className="p-2 mb-5 text-xl font-bold text-justify rounded-lg w-fit sm:text-xl sm:text-left">
+              Links - الروابط
             </p>
 
-            <ul className="flex flex-col items-center gap-6 cursor-pointer w-fit">
-              <li className="transition-colors duration-300 rounded-lg hover:text-orange-400">
-                <a href={"/#deals"}>Home</a>
-              </li>
-              <li className="transition-colors duration-300 rounded-lg hover:text-orange-400">
-                About
-              </li>
-              <li className="transition-colors duration-300 rounded-lg hover:text-orange-400">
-                <button onClick={() => setIsModalOpen(true)} type="submit">
-                  <ContactModal
-                    isOpen={isModalOpen}
-                    onClose={() => setIsModalOpen(false)}
-                  />
-                  Contact Us
-                </button>
-              </li>
-              <li className="transition-colors duration-300 rounded-lg hover:text-orange-400">
-                <Link to={"/login"}>Login</Link>
-              </li>
-            </ul>
-          </div>
+            <span className="transition-colors duration-300 rounded-lg hover:text-orange-400">
+              <a href={"/#deals"}>Home</a>
+            </span>
 
-          {/* Socails */}
-          <div className="flex flex-col items-center px-4 py-8">
-            <p className="p-2 text-xl font-bold text-justify rounded-lg w-fit sm:text-xl sm:text-left">
-              Socials
-            </p>
-            {/* First No. */}
-            <div className="flex items-center gap-3 p-2 mt-3 transition duration-300 rounded-lg hover:text-orange-400">
-              <svg
-                fill="currentColor"
-                className="hover:text-orange-400"
-                height="20px"
-                width="20px"
-                version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 308 308">
-                <g id="SVGRepo_bgCarrier"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"></g>
-                <g id="SVGRepo_iconCarrier">
-                  <g id="XMLID_468_">
-                    <path
-                      id="XMLID_469_"
-                      d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156 c-3.032,0-5.579,1.511-7.563,4.479c-2.243,3.334-9.033,11.271-11.131,13.642c-0.274,0.313-0.648,0.687-0.872,0.687 c-0.201,0-3.676-1.431-4.728-1.888c-24.087-10.463-42.37-35.624-44.877-39.867c-0.358-0.61-0.373-0.887-0.376-0.887 c0.088-0.323,0.898-1.135,1.316-1.554c1.223-1.21,2.548-2.805,3.83-4.348c0.607-0.731,1.215-1.463,1.812-2.153 c1.86-2.164,2.688-3.844,3.648-5.79l0.503-1.011c2.344-4.657,0.342-8.587-0.305-9.856c-0.531-1.062-10.012-23.944-11.02-26.348 c-2.424-5.801-5.627-8.502-10.078-8.502c-0.413,0,0,0-1.732,0.073c-2.109,0.089-13.594,1.601-18.672,4.802 c-5.385,3.395-14.495,14.217-14.495,33.249c0,17.129,10.87,33.302,15.537,39.453c0.116,0.155,0.329,0.47,0.638,0.922 c17.873,26.102,40.154,45.446,62.741,54.469c21.745,8.686,32.042,9.69,37.896,9.69c0.001,0,0.001,0,0.001,0 c2.46,0,4.429-0.193,6.166-0.364l1.102-0.105c7.512-0.666,24.02-9.22,27.775-19.655c2.958-8.219,3.738-17.199,1.77-20.458 C233.168,179.508,230.845,178.393,227.904,176.981z"></path>{" "}
-                    <path
-                      id="XMLID_470_"
-                      d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716 c-0.484,1.429-0.124,3.009,0.933,4.085C1.908,307.58,2.943,308,4,308c0.405,0,0.813-0.061,1.211-0.188l79.92-25.396 c21.87,11.685,46.588,17.853,71.604,17.853C240.143,300.27,308,232.923,308,150.143C308,67.354,240.143,0,156.734,0z M156.734,268.994c-23.539,0-46.338-6.797-65.936-19.657c-0.659-0.433-1.424-0.655-2.194-0.655c-0.407,0-0.815,0.062-1.212,0.188 l-40.035,12.726l12.924-38.129c0.418-1.234,0.209-2.595-0.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677 c0-65.543,53.754-118.867,119.826-118.867c66.064,0,119.812,53.324,119.812,118.867 C276.546,215.678,222.799,268.994,156.734,268.994z"></path>{" "}
-                  </g>
-                </g>
-              </svg>
+            <span className="transition-colors duration-300 rounded-lg hover:text-orange-400">
+              <Link to={"/login"}>Login</Link>
+            </span>
 
-              <Link
-                to={
-                  "https://api.whatsapp.com/send?phone=%2B96878781278&context=ARCm2SgGV0wnZLk0e8GEZq_mvtlzZH7Gxm0Z4m5s1wgQkDNZP4p2Ggsjdp13ohcbjoiZkmWiKyGO2E7bCWQtmhzV5ZcQpPyomFJ2w6aWOuNsbkbTcDKTi2dHT-om5qbt4tKC9t0eBjsnZqfMs9JSaxzXeA&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwZXh0bgNhZW0CMTAAAR0twr5X7PN5CiEANoVJv1UVw9Yzp3Qvh4TVaDhW5i3HcYJpMwAEXJYNHjo_aem_AaybGCUu-9WujHz6XdWDY8vdKnhmosEuR_6KyWWBcLsgNE2DkxbE634bkeGJ4G43HukaD2dOdbeCX4DKrtwzPpD1"
-                }
-                target="_blank"
-                className="font-mono text-nowrap">
-                +96878781278
-              </Link>
-            </div>
-
-            {/* Second No. */}
-            <div className="flex items-center gap-3 p-2 mt-3 transition duration-300 rounded-lg hover:text-orange-400">
-              <Phone />
-              <Link to={"tel:+96897310806"} className="font-mono text-nowrap ">
-                +96897310806
-              </Link>
-            </div>
-
-            {/* Socials */}
-            <div className="flex items-center gap-5 p-2 mt-6 rounded-lg">
+            {/* Social Media Logos */}
+            <div className="flex items-center gap-5 p-2 rounded-lg">
               <Link
                 to={"https://www.instagram.com/apizza_om/"}
                 target="_blank"
@@ -203,6 +146,70 @@ const Footer = () => {
                   />
                 </svg>
               </Link>
+            </div>
+          </div>
+
+          {/* Contact */}
+          <div className="flex flex-col items-center gap-2 px-4 py-8">
+            <p className="p-2 mb-5 text-xl font-bold text-justify rounded-lg w-fit sm:text-xl sm:text-left">
+              Socials - التواصل
+            </p>
+            {/* First No. */}
+            <div className="flex items-center gap-3 p-2 transition duration-300 rounded-lg hover:text-orange-400">
+              <svg
+                fill="currentColor"
+                className="hover:text-orange-400"
+                height="20px"
+                width="20px"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 308 308">
+                <g id="SVGRepo_bgCarrier"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <g id="XMLID_468_">
+                    <path
+                      id="XMLID_469_"
+                      d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156 c-3.032,0-5.579,1.511-7.563,4.479c-2.243,3.334-9.033,11.271-11.131,13.642c-0.274,0.313-0.648,0.687-0.872,0.687 c-0.201,0-3.676-1.431-4.728-1.888c-24.087-10.463-42.37-35.624-44.877-39.867c-0.358-0.61-0.373-0.887-0.376-0.887 c0.088-0.323,0.898-1.135,1.316-1.554c1.223-1.21,2.548-2.805,3.83-4.348c0.607-0.731,1.215-1.463,1.812-2.153 c1.86-2.164,2.688-3.844,3.648-5.79l0.503-1.011c2.344-4.657,0.342-8.587-0.305-9.856c-0.531-1.062-10.012-23.944-11.02-26.348 c-2.424-5.801-5.627-8.502-10.078-8.502c-0.413,0,0,0-1.732,0.073c-2.109,0.089-13.594,1.601-18.672,4.802 c-5.385,3.395-14.495,14.217-14.495,33.249c0,17.129,10.87,33.302,15.537,39.453c0.116,0.155,0.329,0.47,0.638,0.922 c17.873,26.102,40.154,45.446,62.741,54.469c21.745,8.686,32.042,9.69,37.896,9.69c0.001,0,0.001,0,0.001,0 c2.46,0,4.429-0.193,6.166-0.364l1.102-0.105c7.512-0.666,24.02-9.22,27.775-19.655c2.958-8.219,3.738-17.199,1.77-20.458 C233.168,179.508,230.845,178.393,227.904,176.981z"></path>{" "}
+                    <path
+                      id="XMLID_470_"
+                      d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716 c-0.484,1.429-0.124,3.009,0.933,4.085C1.908,307.58,2.943,308,4,308c0.405,0,0.813-0.061,1.211-0.188l79.92-25.396 c21.87,11.685,46.588,17.853,71.604,17.853C240.143,300.27,308,232.923,308,150.143C308,67.354,240.143,0,156.734,0z M156.734,268.994c-23.539,0-46.338-6.797-65.936-19.657c-0.659-0.433-1.424-0.655-2.194-0.655c-0.407,0-0.815,0.062-1.212,0.188 l-40.035,12.726l12.924-38.129c0.418-1.234,0.209-2.595-0.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677 c0-65.543,53.754-118.867,119.826-118.867c66.064,0,119.812,53.324,119.812,118.867 C276.546,215.678,222.799,268.994,156.734,268.994z"></path>{" "}
+                  </g>
+                </g>
+              </svg>
+
+              <Link
+                to={
+                  "https://api.whatsapp.com/send?phone=%2B96878781278&context=ARCm2SgGV0wnZLk0e8GEZq_mvtlzZH7Gxm0Z4m5s1wgQkDNZP4p2Ggsjdp13ohcbjoiZkmWiKyGO2E7bCWQtmhzV5ZcQpPyomFJ2w6aWOuNsbkbTcDKTi2dHT-om5qbt4tKC9t0eBjsnZqfMs9JSaxzXeA&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwZXh0bgNhZW0CMTAAAR0twr5X7PN5CiEANoVJv1UVw9Yzp3Qvh4TVaDhW5i3HcYJpMwAEXJYNHjo_aem_AaybGCUu-9WujHz6XdWDY8vdKnhmosEuR_6KyWWBcLsgNE2DkxbE634bkeGJ4G43HukaD2dOdbeCX4DKrtwzPpD1"
+                }
+                target="_blank"
+                className="font-mono text-nowrap">
+                +96878781278
+              </Link>
+            </div>
+
+            {/* Second No. */}
+            <div className="flex items-center gap-3 p-2 transition duration-300 rounded-lg hover:text-orange-400">
+              <Phone />
+              <Link to={"tel:+96897310806"} className="font-mono text-nowrap ">
+                +96897310806
+              </Link>
+            </div>
+
+            {/* Send Email */}
+            <div className="flex items-center gap-3 p-2 transition duration-300 rounded-lg hover:text-orange-400">
+              <MailPlus />
+              <button onClick={() => setIsModalOpen(true)} type="submit">
+                <ContactModal
+                  isOpen={isModalOpen}
+                  onClose={() => setIsModalOpen(false)}
+                />
+                Send Email
+              </button>
             </div>
           </div>
         </div>
